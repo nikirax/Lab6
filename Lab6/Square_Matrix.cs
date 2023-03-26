b2725ae0dc7cf186e57eae811e062fed57ed6cb3
User <User@WIN-2FMQ0M18AC7>
2023-03-26 13:35:15 +0300

    Добавьте файлы проекта.

---
+++ Lab6/Square_Matrix.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6
+{
+    public class Square_Matrix
+    {
+        private double[,] matrix { get; set; }
+        public int Lenght() { return matrix.Length; }
+        public double getValue(int pos_y, int pos_x)
+        {
+            return matrix[pos_y, pos_x];
+        }
+        public void setValue(int pos_y, int pos_x, double value)
+        {
+            matrix[pos_y, pos_x] = value;
+        }
+        public Square_Matrix(int y, int x) { this.matrix = new double[y, x]; }
+        public Square_Matrix(double[,] matrix)
+        {
+            this.matrix = matrix;
+        }
+        public double Multi_Matrix(params int[] items)
+        {
+            if (items.Length < 1)
+            {
+                return Plus_Matrix();
+            }
+            else if (items.Length > 1)
+            {
+                return 0.0;
+            }
+            else
+            {
+                if (items[0] < 0)
+                {
+                    return Mult(Math.Abs(items[0]), 0);
+                }
+                else if (items[0] > 0)
+                {
+                    return Mult(0, items[0]);
+                }
+                else
+                {
+                    return Mult(0, 0);
+                }
+            }
+        }
+        private double Plus_Matrix()
+        {
+            double sum = 0;
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix.Length; j++)
+                {
+                    sum += matrix[i, j];
+                }
+            }
+            return sum;
+        }
+        private double Mult(int i, int j)
+        {
+            bool main = i == 0 && j == 0 ? true : false;
+            double mult = 1;
+            for (; i < matrix.GetLength(0); i++)
+            {
+                if (main)
+                {
+                    mult *= matrix[i, i];
+                    continue;
+                }
+                mult *= matrix[i, j];
+            }
+            return mult;
+        }
+    }
+}
---
+++ Test/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Lab6;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMatrixLenght()
+        {
+            double[,] matrix = new double[5, 5];
+            var square_matrix = new Square_Matrix(5, 5);
+
+            Assert.AreEqual(matrix.Length, square_matrix.Lenght());
+        }
+        [TestMethod]
+        public void TestMatrixPlus()
+        {
+            double mult_matrix = 1;
+            double[,] matrix = new double[5, 5]
+            {
+                {1, 2, 3, 4, 5 },
+                {1, 2, 3, 4, 5 },
+                {1, 2, 3, 4, 5 },
+                {1, 2, 3, 4, 5 },
+                {1, 2, 3, 4, 5 }
+            };
+            var square_matrix = new Square_Matrix(matrix);
+            
+            for(var i = 0; i < 5; i++)
+            {
+                mult_matrix *= matrix[i, i];
+            }
+            Assert.AreEqual(mult_matrix, square_matrix.Multi_Matrix(0));
+        }
+    }
+}
